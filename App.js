9870080075af206f70e07a6800b0b5f2659a916c
Michael Lin <mlzc@hey.com>
2021-09-17 14:21:22 -0700

    Add notification test

--- App.js
+++ App.js
@@ -2,12 +2,64 @@ import 'expo-dev-client';
 
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+  const [token, setToken] = React.useState(null);
+
+  const sendNotificationAsync = React.useCallback(
+    async (title, body) => {
+      const message = {
+        to: token,
+        sound: 'default',
+        title,
+        body,
+        data: {someData: 'goes here'}
+      }
+      console.log('token', token)
+      console.log(`Sending notification: ${JSON.stringify(message)}`)
+
+      const resp = await fetch('https://exp.host/--/api/v2/push/send', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Accept-encoding': 'gzip, deflate',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(message)
+      })
+      console.log(await resp.json())
+    },
+    [token]
+  )
+
+  registerForPushNotificationsAsync = async () => {
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      const token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+      setToken(token)
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Test Notification" onPress={() => sendNotificationAsync("hi", "this is a test")} />
       <StatusBar style="auto" />
     </View>
   );
